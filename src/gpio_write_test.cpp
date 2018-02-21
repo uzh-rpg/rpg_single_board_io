501e7d96c59293e0903532f1089823b889972ba7
mfaessle <matfaessler@gmail.com>
2018-02-21 18:30:43 +0100

    added links to board pinouts

--- src/gpio_write_test.cpp
+++ src/gpio_write_test.cpp
@@ -8,8 +8,13 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
 
   const unsigned int num_gpio = 25; // Example GPIO for Odroid XU4
+  // See: https://wiki.odroid.com/odroid-xu4/hardware/expansion_connectors
+
   //const unsigned int num_gpio = 200; // Example GPIO for Odroid U3
+  // See: http://www.hardkernel.com/main/products/prdt_info.php?g_code=G138745696275&tab_idx=2
+
   //const unsigned int num_gpio = 3; // Example GPIO for Up Board
+  // See: https://wiki.up-community.org/Pinout
 
   rpg_single_board_io::GPIO gpio(num_gpio,
                                  rpg_single_board_io::GpioDirection::Out);
